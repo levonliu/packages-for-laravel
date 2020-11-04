f854fdf5add68c3c88df1df6111593773a600252
liuwen <levonliu@outlook.com>
2020-11-04 20:54:09 +0800

    commit

--- src/Config/request.php
+++ src/Config/request.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'base_uri'     => env('REQUEST_BASE_URI'),
-    'curl_timeout' => env('REQUEST_CURL_TIMEOUT'),
-    'verify'       => env('REQUEST_VERIFY')
+    'curl_timeout' => env('REQUEST_CURL_TIMEOUT', 240),
+    'verify'       => env('REQUEST_VERIFY', FALSE)
 ];
